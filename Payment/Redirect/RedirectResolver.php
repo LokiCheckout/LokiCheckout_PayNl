786ca32e04e96a5aec1f8bc45403fdcbf9b0a96f
Jisse Reitsma <jisse@yireo.com>
2025-03-28 10:50:25 +0100

    Refactor PaymentRedirectUrl plugin to resolver

---
+++ Payment/Redirect/RedirectResolver.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Yireo\LokiCheckoutPayNl\Payment\Redirect;
+
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Model\Order;
+use Paynl\Payment\Model\Paymentmethod\Paymentmethod;
+use Yireo\LokiCheckout\Payment\Redirect\RedirectResolverInterface;
+use Yireo\LokiCheckout\Step\FinalStep\RedirectContext;
+
+class RedirectResolver implements RedirectResolverInterface
+{
+    public function resolve(RedirectContext $redirectContext): false|string
+    {
+        $paymentMethod = $redirectContext->getPaymentMethod();
+        if (false === $paymentMethod instanceof Paymentmethod) {
+            return false;
+        }
+
+        /** @var Order $order */
+        $order = $redirectContext->getOrder();
+        if (false === $order instanceof OrderInterface) {
+            return false;
+        }
+
+        return $paymentMethod->startTransaction($order);
+    }
+}
