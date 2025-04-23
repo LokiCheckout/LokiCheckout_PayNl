3321eb7bab7922a895d2db9d2d497b957a727ea9
Jisse Reitsma <jisse@yireo.com>
2025-04-23 16:44:40 +0200

    Draft of fast checkout option for PSPs

--- view/frontend/tailwind/tailwind.config.js
+++ view/frontend/tailwind/tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     content: [
         "../../../etc/loki_checkout.xml",
+        "../templates/*.phtml",
+        "../templates/**/*.phtml",
         "../layout/*.xml",
     ],
 };
