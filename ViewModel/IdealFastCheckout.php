6822ef17951c41dc3f666433fb81fc0b4d72b950
Jisse Reitsma <jisse@yireo.com>
2025-05-15 13:44:10 +0200

    Rename FastCheckout to iDeal Fast Checkout

---
+++ ViewModel/IdealFastCheckout.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace Yireo\LokiCheckoutPayNl\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Magento\Quote\Api\Data\AddressInterface;
+use Paynl\Payment\ViewModel\FastCheckout;
+use Yireo\LokiCheckout\ViewModel\CheckoutState;
+
+class IdealFastCheckout implements ArgumentInterface
+{
+    public function __construct(
+        private FastCheckout $fastCheckout,
+        private CheckoutState $checkoutState,
+    ) {
+    }
+
+    public function enabled(): bool
+    {
+        return $this->fastCheckout->getVisibility();
+    }
+
+    public function modalEnabled(): bool
+    {
+        return $this->fastCheckout->modalEnabled();
+    }
+
+    public function getShippingAddress(): AddressInterface
+    {
+        return $this->checkoutState->getQuote()->getShippingAddress();
+    }
+}
