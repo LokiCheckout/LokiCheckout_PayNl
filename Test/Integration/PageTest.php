51ec915b9732dbb04c2fe07798514c90920fcf65
Jisse Reitsma <jisse@yireo.com>
2025-05-19 14:25:23 +0200

    Fix test

--- Test/Integration/PageTest.php
+++ Test/Integration/PageTest.php
@@ -5,19 +5,25 @@ declare(strict_types=1);
 namespace Yireo\LokiCheckoutPayNl\Test\Integration;
 
 use Magento\Catalog\Test\Fixture\Product as ProductFixture;
+use Magento\Quote\Test\Fixture\AddProductToCart as AddProductToCartFixture;
 use Magento\Quote\Test\Fixture\GuestCart as GuestCartFixture;
+use Magento\TestFramework\Fixture\AppArea;
 use Magento\TestFramework\Fixture\DataFixture;
 use Yireo\IntegrationTestHelper\Test\Integration\Traits\AssertModuleIsEnabled;
 use Yireo\IntegrationTestHelper\Test\Integration\Traits\AssertModuleIsRegistered;
 use Yireo\LokiCheckout\Test\Integration\LokiCheckoutPageTestCase;
 
+#[
+    DataFixture(ProductFixture::class, ['sku' => 'simple-product-001'], as: 'product'),
+    DataFixture(GuestCartFixture::class, as: 'cart'),
+    DataFixture(AddProductToCartFixture::class, ['cart_id' => '$cart.id$', 'product_id' => '$product.id$']),
+    AppArea('frontend')
+]
 final class PageTest extends LokiCheckoutPageTestCase
 {
     use AssertModuleIsEnabled;
     use AssertModuleIsRegistered;
 
-    #[DataFixture(ProductFixture::class, ['sku' => 'simple-product-001'], as: 'product')]
-    #[DataFixture(GuestCartFixture::class, as: 'cart')]
     final public function testForCss(): void
     {
         $body = (string)$this->getResponse()->getBody();
