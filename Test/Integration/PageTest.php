e36f4a2acf10dcf4576cee3ee9e4fa935dad2b35
Jisse Reitsma <jisse@yireo.com>
2025-11-21 10:26:45 +0100

    Manually execute dispatch to checkout

--- Test/Integration/PageTest.php
+++ Test/Integration/PageTest.php
@@ -4,28 +4,29 @@ declare(strict_types=1);
 
 namespace LokiCheckout\PayNl\Test\Integration;
 
-use Magento\Catalog\Test\Fixture\Product as ProductFixture;
+use LokiCheckout\Core\Test\Integration\LokiCheckoutTestCase;
+use Magento\Catalog\Test\Fixture\Product;
 use Magento\Quote\Test\Fixture\AddProductToCart as AddProductToCartFixture;
 use Magento\Quote\Test\Fixture\GuestCart as GuestCartFixture;
 use Magento\TestFramework\Fixture\AppArea;
 use Magento\TestFramework\Fixture\DataFixture;
 use Yireo\IntegrationTestHelper\Test\Integration\Traits\AssertModuleIsEnabled;
 use Yireo\IntegrationTestHelper\Test\Integration\Traits\AssertModuleIsRegistered;
-use LokiCheckout\Core\Test\Integration\LokiCheckoutPageTestCase;
 
 #[
-    DataFixture(ProductFixture::class, ['sku' => 'simple-product-001'], as: 'product'),
+    DataFixture(Product::class, as: 'product'),
     DataFixture(GuestCartFixture::class, as: 'cart'),
     DataFixture(AddProductToCartFixture::class, ['cart_id' => '$cart.id$', 'product_id' => '$product.id$']),
     AppArea('frontend')
 ]
-final class PageTest extends LokiCheckoutPageTestCase
+class PageTest extends LokiCheckoutTestCase
 {
     use AssertModuleIsEnabled;
     use AssertModuleIsRegistered;
 
     final public function testForCss(): void
     {
+        $this->dispatchToCheckout();
         $body = (string)$this->getResponse()->getBody();
         $this->assertStringNotContainsString('css/payFastCheckout.css', $body);
         $this->assertStringNotContainsString('css/payCheckout.css', $body);
