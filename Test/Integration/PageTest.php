cdc8528fa8b7c8c87db0de0fb191326f5aab8a81
Jisse Reitsma <jisse@yireo.com>
2025-12-06 14:52:18 +0100

    Cleanup integration tests

--- Test/Integration/PageTest.php
+++ Test/Integration/PageTest.php
@@ -5,32 +5,20 @@ declare(strict_types=1);
 namespace LokiCheckout\PayNl\Test\Integration;
 
 use LokiCheckout\Core\Test\Integration\LokiCheckoutTestCase;
-use Magento\Catalog\Test\Fixture\Product;
+use Magento\Catalog\Test\Fixture\Product as ProductFixture;
 use Magento\Quote\Test\Fixture\AddProductToCart as AddProductToCartFixture;
 use Magento\Quote\Test\Fixture\GuestCart as GuestCartFixture;
 use Magento\TestFramework\Fixture\AppArea;
 use Magento\TestFramework\Fixture\DataFixture;
-use Yireo\IntegrationTestHelper\Test\Integration\Traits\AssertModuleIsEnabled;
-use Yireo\IntegrationTestHelper\Test\Integration\Traits\AssertModuleIsRegistered;
 
 #[
-    DataFixture(Product::class, as: 'product'),
+    DataFixture(ProductFixture::class, ['sku' => 'simple-product-001'], as: 'product'),
     DataFixture(GuestCartFixture::class, as: 'cart'),
     DataFixture(AddProductToCartFixture::class, ['cart_id' => '$cart.id$', 'product_id' => '$product.id$']),
-    AppArea('frontend')
+    AppArea('frontend'),
 ]
 class PageTest extends LokiCheckoutTestCase
 {
-    use AssertModuleIsEnabled;
-    use AssertModuleIsRegistered;
-
-    #[
-        DataFixture(Product::class, as: 'product'),
-        DataFixture(GuestCartFixture::class, as: 'cart'),
-        DataFixture(AddProductToCartFixture::class, ['cart_id' => '$cart.id$', 'product_id' => '$product.id$']),
-        AppArea('frontend')
-    ]
-
     final public function testForCss(): void
     {
         $this->dispatchToCheckout();
--- Test/Integration/ViewModel/IdealFastCheckoutTest.php
+++ Test/Integration/ViewModel/IdealFastCheckoutTest.php
@@ -4,30 +4,21 @@ declare(strict_types=1);
 
 namespace LokiCheckout\PayNl\Test\Integration\ViewModel;
 
+use Magento\Customer\Api\Data\CustomerInterface;
+use Magento\Customer\Model\Customer;
 use Magento\Customer\Model\Session as CustomerSession;
 use Magento\Customer\Test\Fixture\Customer as CustomerFixture;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\TestFramework\Fixture\Config as ConfigFixture;
 use Magento\TestFramework\Fixture\DataFixture;
-use Magento\TestFramework\Fixture\DataFixtureStorage;
 use Magento\TestFramework\Fixture\DataFixtureStorageManager;
-use Magento\TestFramework\Helper\Bootstrap;
 use PHPUnit\Framework\TestCase;
 use Yireo\IntegrationTestHelper\Test\Integration\Traits\GetObjectManager;
 use LokiCheckout\PayNl\ViewModel\IdealFastCheckout;
-use Magento\TestFramework\ObjectManager;
 
-final class IdealFastCheckoutTest extends TestCase
+class IdealFastCheckoutTest extends TestCase
 {
     use GetObjectManager;
-
-    private ?ObjectManager $objectManager = null;
-    private ?DataFixtureStorage $fixtures = null;
-
-    protected function setUp(): void
-    {
-        $this->fixtures = $this->getObjectManager()->get(DataFixtureStorageManager::class)->getStorage();
-    }
 
     final public function testInstantiation(): void
     {
@@ -44,15 +35,15 @@ final class IdealFastCheckoutTest extends TestCase
         $this->assertEquals(true, $actual);
     }
 
-    #[DataFixture(CustomerFixture::class, as: 'customer')]
-    #[ConfigFixture('payment/paynl_payment_ideal/fast_checkout_guest_only', 1, 'store', 'default')]
+    #[
+        DataFixture(CustomerFixture::class, ['email' => 'info@example.com'], as: 'customer'),
+        ConfigFixture('payment/paynl_payment_ideal/fast_checkout_guest_only', 1, 'store', 'default')
+    ]
     final public function testIsDisabled(): void
     {
-        //$fixtures = DataFixtureStorageManager::getStorage();
-        $customer = $this->fixtures->get('customer');
-        if (empty($customer)) {
-            $this->fail('Customer fixture failed loading');
-        }
+        $fixtures = DataFixtureStorageManager::getStorage();
+        $customer = $fixtures->get('customer');
+        $this->assertInstanceOf(Customer::class, $customer, get_class($customer));
 
         $customerSession = $this->getObjectManager()->get(CustomerSession::class);
         $customerSession->loginById($customer->getId());
@@ -76,6 +67,7 @@ final class IdealFastCheckoutTest extends TestCase
     {
         $scopeConfig = $this->getObjectManager()->get(ScopeConfigInterface::class);
         $this->assertEquals(0, $scopeConfig->getValue('payment/paynl_payment_ideal/fast_checkout_show_modal'));
+
         $idealFastCheckout = $this->getInstance();
         $actual = $idealFastCheckout->modalEnabled();
         $this->assertEquals(false, $actual);
